49fdc9e81f9d41c5251a2cc962bc54b9412d003b
Bilal Irfan <bi0755562@gmail.com>
2025-05-19 23:10:49 +0500

    Project Complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientProviders from "@/components/ClientProviders";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
